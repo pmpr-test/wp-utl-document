decdb29541490a50fc0d993fbf7c43902ef0457d
pmpr.dev <pmpr.dev@gmail.com>
2024-09-14 19:44:19 +0000

    Obfuscated

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+/*   _______________________________________
+    |  Obfuscated by PMPR - Php Obfuscator  |
+    |             66e5e76faaa90             |
+    |_______________________________________|
+*/
+ use Pmpr\Utility\Document\Document; use Pmpr\Utility\Document\PDF; Document::symcgieuakksimmu(); if (!function_exists("\160\x72\x5f\x75\164\151\x6c\151\x74\x79\137\x64\157\143\165\x6d\x65\x6e\164\x5f\147\145\x74\x5f\160\x64\x66")) { function pr_utility_document_get_pdf() : PDF { return PDF::symcgieuakksimmu(); } }
---
+++ src/Pmpr/Utility/Document/Container.php
@@ -0,0 +1,7 @@
+<?php
+/*   _______________________________________
+    |  Obfuscated by PMPR - Php Obfuscator  |
+    |             66e5e76faaa90             |
+    |_______________________________________|
+*/
+ namespace Pmpr\Utility\Document; use Pmpr\Common\Foundation\Container\LightContainer; class Container extends LightContainer { }
